a661824e1673edf511e977ccc80c92ef0b98673e
Reginaldo Bertozzi <65985494+Reginaldo-Bertozzi@users.noreply.github.com>
2023-06-03 11:20:49 -0300

    Este projeto srve de exemplo da utilização do intent para transferir valores de uma activity para outra

--- app/src/main/java/com/elygium/intentcomextras/MainActivity.kt
+++ app/src/main/java/com/elygium/intentcomextras/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.elygium.intentcomextras
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.elygium.intentcomextras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOk.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+
+            val i = Intent(this, MainActivity2::class.java)
+            i.putExtra("nome", nome)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/elygium/intentcomextras/MainActivity2.kt
+++ app/src/main/java/com/elygium/intentcomextras/MainActivity2.kt
@@ -2,10 +2,26 @@ package com.elygium.intentcomextras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.elygium.intentcomextras.databinding.ActivityMain2Binding
+import com.elygium.intentcomextras.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val i = intent
+        val nome = i.extras?.getString("nome")
+
+        if (nome.equals("") || nome == null) {
+            Toast.makeText(applicationContext, "Nome não inserido", Toast.LENGTH_SHORT).show()
+        } else {
+            binding.textNome.setText("Olá $nome")
+        }
     }
 }
